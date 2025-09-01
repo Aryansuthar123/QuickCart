2cb27bc880abfc46defc9db0393ce3bcf6cf0752
Aryansuthar123 <aryansuthar222@gmail.com>
2025-09-01 19:20:38 +0530

    first comit

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
+  reactStrictMode: true, // React ke strict checks enable
+  swcMinify: true,       // Fast build with SWC
+
+  // Remote images allow karne ke liye
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+
+  // Agar turbopack issue de raha hai to isko OFF rakho
+  experimental: {
+    turbo: {
+      enabled: false,
     },
+  },
 };
 
 export default nextConfig;
